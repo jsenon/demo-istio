bb4aa7615a68af9e98c718fb9b22be6ee1b33292
Julien SENON <julien.senon@gmail.com>
2018-06-12 16:35:32 +0200

    Add shell script

---
+++ k8s/cleaning.sh
@@ -0,0 +1,11 @@
+#/bin/zsh
+
+kubectl delete -f demo1/
+kubectl delete -f demo2/
+kubectl delete -f demo3/
+kubectl delete -f demo4/
+kubectl delete -f demo5/
+kubectl delete -f demo6/
+kubectl delete -f demo7/
+kubectl delete ns mtls
+kubectl delete ns legacy
