69507afa08b113a94266ae90e79109b2b98a382c
Julien Senon <julien.senon@gmail.com>
2019-03-17 20:52:44 +0100

    Update demo4 circuit breaker

--- k8s/demo4/demo4.sh
+++ k8s/demo4/demo4.sh
@@ -4,9 +4,14 @@ istioctl delete -f ../demo2/virtualservice-play.yml
 istioctl delete -f ../demo3/virtualservice-canary.yml
 
 istioctl create -f virtualservice-delay.yml
+
+kubectl apply -f ../demo3/deployment_v002.yml
+kubectl apply -f ../demo3/service_v2.yml
+
+kubectl apply -f ../demo4/circuit-breaker.yml
+
 istioctl create -f outlier.yml
 
-kubectl apply -f ../demo3/service_v2.yml
-kubectl apply -f ../demo3/deployment_v002.yml
+
 
 
